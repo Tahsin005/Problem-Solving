0be1b23439470e2ee7dd1712cdd9b8cd58e5fe5d
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-07 02:08:54 +0600

    day - 149

---
+++ Week 26+/H_A_B_Strikes_Back.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define pi 3.14159265359
+#define mapo max_element(v.begin(), v.end()) - v.begin()
+#define ll long long
+#define lcm(a, b) (a * b) / __gcd(a, b)
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b;
+    cout << a + b;
+
+    return 0;
+}
