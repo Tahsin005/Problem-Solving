62bba274140b68db0077175b55383699b7abb1ca
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-15 16:01:39 +0600

    day - 159

---
+++ Week 26+/C_WTF.cpp
@@ -0,0 +1,3 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {int i, n; double a, b = 0, mx = 0; cin >> n; for (i = 1; i <= n; i++){ cin >> a; b += a; mx = max(mx, b / i);} cout << mx << '\n';}
