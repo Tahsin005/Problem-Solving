79a5eb2a5d5dfa246f5c24dbbf1980cf210c8866
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-01-21 11:03:30 +0600

    day - 163

--- Week 26+/B_A_Map_of_the_Cat.py
+++ Week 26+/B_A_Map_of_the_Cat.py
@@ -1,8 +1,1 @@
-print(9)
-
-user_input = input()
-
-if user_input[-2:] in 'lenuseay':
-    print('ngorrummaply'[::2])
-else:
-    print('ngorrummaply'[1::2])
+print(9);print('ngorrummaply'[input()[-2:]in'lenuseay'::2])
