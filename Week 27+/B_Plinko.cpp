7c7086eb22b8701052da48ae4512da5eb8e3a300
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-04 02:00:50 +0600

    day - 236

---
+++ Week 27+/B_Plinko.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout << "-10000";
+    return 0;
+}
