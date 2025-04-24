89e65ec52989305cc86ad5e2e88d74ca33a85244
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-24 08:10:10 +0600

    day - 256

---
+++ Week 27+/A_Common_Multiple.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	in = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func main () {
+	var t, n, num int
+	fmt.Fscan(in, &t)
+
+	for range t {
+		fmt.Fscan(in, &n)
+		mp := map[int]bool{}
+
+		res := 0
+
+		for range n {
+			fmt.Fscan(in, &num)
+			_, ok := mp[num]
+			if !ok {
+				res++
+				mp[num] = true
+			}
+		}
+
+		fmt.Println(res)
+	}
+}
