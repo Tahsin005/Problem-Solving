11d2ec0f71457ab620fa313e9289fc07d100bc40
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-30 20:39:01 +0600

    day - 2612

---
+++ Week 27+/A_Max_and_Mod.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var t int
+	fmt.Scan(&t)
+	for ; t > 0; t-- {
+		var n int
+		fmt.Scan(&n)
+		if n & 1 == 1 {
+			fmt.Print(n)
+			for i := 1; i < n; i++ {
+				fmt.Print(" ", i)
+			}
+			fmt.Println()
+		} else {
+			fmt.Println(-1)
+		}
+	}
+}
