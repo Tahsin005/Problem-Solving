4d67028a6c7e3acd2153213ab19408c2999a5442
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-06-23 07:32:14 +0600

    day - 316

---
+++ Week 27+/B_The_String_Has_a_Target.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+    var s string
+	fmt.Scan(&n, &s)
+	p := 0
+	for i := 1; i < n; i++ {
+		if s[i] <= s[p] {
+			p = i
+		}
+	}
+	fmt.Println(string(s[p]), s[:p], s[p + 1:])
+}
