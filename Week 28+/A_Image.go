cdd77109d20e4675d7cbe9e7302c9d834af2e751
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-07-31 07:27:08 +0600

    day - 354

---
+++ Week 28+/A_Image.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var t int
+	_, _ = fmt.Scan(&t)
+
+	for i := 0; i < t; i++ {
+		var (
+			p1 string
+			p2 string
+		)
+		_, _ = fmt.Scan(&p1, &p2)
+
+		m := make(map[rune]int)
+
+		for _, w := range p1 + p2 {
+			m[w]++
+		}
+
+		fmt.Println(len(m) - 1)
+	}
+}
