52c4b1e038c861c354128438dd06a1d1a1631a40
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-11 02:08:17 +0600

    day - 365

---
+++ Week 28+/A_Lever.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func solve() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+	for i := 0; i < n; i++ {
+		fmt.Scan(&b[i])
+	}
+
+	var ans int = 1
+	for i := 0; i < n; i++ {
+		if a[i] > b[i] {
+			ans += a[i] - b[i]
+		}
+	}
+	fmt.Println(ans)
+}
+
+func main() {
+	var t int
+	fmt.Scan(&t)
+	for range t {
+		solve()
+	}
+}
