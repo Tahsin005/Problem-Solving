47443af8c71f967c49f4e7c5edcb29e6a21a8c09
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-11-16 21:27:26 +0600

    day-5

---
+++ A_Mezo_Playing_Zoma.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    cout << n + 1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    solve();
+}
