814269aeab1ff19c34d88903aa200fe47476ee9c
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-11-18 15:52:09 +0600

    day-7

---
+++ MonkandtheMagicalCandyBags.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+void solve(){
+    ll n, k;
+    cin >> n >> k;
+    
+    multiset<ll> s;
+    for (ll i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        
+        s.insert(x);
+    }
+    
+    ll cnt = 0;
+    while (k--) {
+        if (s.size() > 0) {
+            auto it = prev(s.end());
+            cnt += (*it);
+            
+            ll new_value = (*it) / 2;
+            s.erase(it);
+            s.insert(new_value);
+        }
+    }
+    
+    cout << cnt << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int tt; cin >> tt; while (tt--) solve();
+}
