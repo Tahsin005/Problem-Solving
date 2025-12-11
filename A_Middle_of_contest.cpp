16ab807ac67259bbe930875a0172b3af85e90c92
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-12-11 10:20:52 +0600

    day-30

---
+++ A_Middle_of_contest.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e18+7;
+
+void solve() {
+    int h1, m1;
+	scanf("%d:%d", &h1, &m1);
+	int h2, m2;
+	scanf("%d:%d", &h2, &m2);
+	int t1 = h1 * 60 + m1;
+	int t2 = h2 * 60 + m2;
+	int t3 = (t1 + t2) / 2;
+	printf("%02d:%02d\n", t3 / 60, t3 % 60);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1; while (tt--) solve();
+}
