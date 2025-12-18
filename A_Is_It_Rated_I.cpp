feb75635d60e215b85d09a58e250ec1f0b89eb62
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-12-18 09:42:38 +0600

    day-37

---
+++ A_Is_It_Rated_I.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e18+7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    while (cin >> s) {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
