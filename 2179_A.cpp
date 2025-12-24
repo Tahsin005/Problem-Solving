ba2f40ce6543855a2c5032a9e6791a5f486a5082
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2025-12-24 15:17:01 +0600

    Implement main function and SUIII for input/output

---
+++ 2179_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M = 1e18+7;
+ 
+void SUIII() {
+    int k, x;
+    cin >> k >> x;
+    cout << k * x + 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1; 
+    cin >> tt;
+    while (tt--) {
+        SUIII();
+    }
+    
+    return 0;
+}
